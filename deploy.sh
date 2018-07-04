55623813055097229d2260d8256eb6d71273ebca
buddha314 <buddha_314@yahoo.com>
2018-07-04 06:14:42 -0700

    updating deploy

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,37 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-#DIR=$(dirname "$0")
+# This script allows you to easily and quickly generate and deploy your website
+# using Hugo to your personal GitHub Pages repository. This script requires a
+# certain configuration, run the `setup.sh` script to configure this. See
+# https://hjdskes.github.io/blog/deploying-hugo-on-personal-github-pages/index.html
+# for more information.
 
-#cd $DIR/..
+# Set the English locale for the `date` command.
+export LC_TIME=en_US.UTF-8
 
-if [[ $(git status -s) ]]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
+# GitHub username.
+USERNAME=hjdskes
+# Name of the branch containing the Hugo source files.
+SOURCE=hugo
+# The commit message.
+MESSAGE="Site rebuild $(date)"
 
-echo "Deleting old publication"
-rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
+msg() {
+    printf "\033[1;32m :: %s\n\033[0m" "$1"
+}
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
+msg "Pulling down the \`master\` branch into \`public\` to help avoid merge conflicts"
+git subtree pull --prefix=public \
+	git@github.com:$USERNAME/$USERNAME.github.io.git master -m "Merge master"
 
-echo "Removing existing files"
-rm -rf public/*
+msg "Building the website"
+hugo
 
-echo "Generating site"
-HUGO_ENV="production" hugo
+msg "Pushing the updated \`public\` folder to the \`$SOURCE\` branch"
+git add public
+git commit -m "$MESSAGE"
+git push origin "$SOURCE"
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
-
-echo "pushing to publish"
-git push origin master -f
+msg "Pushing the updated \`public\` folder to the \`master\` branch"
+git subtree push --prefix=public \
+	git@github.com:$USERNAME/$USERNAME.github.io.git master
