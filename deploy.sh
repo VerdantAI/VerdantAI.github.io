eee5344fc23831ed3dfa25fb8171fe26bdb4530a
buddha314 <buddha_314@yahoo.com>
2018-07-04 06:10:50 -0700

    trying to load the develop branch again

--- deploy.sh
+++ deploy.sh
@@ -1,37 +1,32 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-# This script allows you to easily and quickly generate and deploy your website
-# using Hugo to your personal GitHub Pages repository. This script requires a
-# certain configuration, run the `setup.sh` script to configure this. See
-# https://hjdskes.github.io/blog/update-deploying-hugo-on-personal-github-pages/
-# for more information.
+#DIR=$(dirname "$0")
 
-# Set the English locale for the `date` command.
-export LC_TIME=en_US.UTF-8
+#cd $DIR/..
 
-# The commit message.
-MESSAGE="Site rebuild $(date)"
-
-msg() {
-    printf "\033[1;32m :: %s\n\033[0m" "$1"
-}
-
-if [[ $(git status -s) ]]; then
-    msg "The working directory is dirty, please commit or stash any pending changes"
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
     exit 1;
 fi
 
-msg "Removing the old website"
-pushd public
-git rm -rf *
-popd
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
 
-msg "Building the website"
-hugo
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public origin/gh-pages
 
-msg "Pushing the updated \`public\` folder to the \`master\` branch"
-pushd public
-git add *
-git commit -m "$MESSAGE"
-popd
-git push origin master
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+HUGO_ENV="production" hugo
+
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+
+echo "pushing to publish"
+git push origin master -f
